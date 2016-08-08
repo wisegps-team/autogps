1aad7254ede5f3f41dab10103a4c44026b336d0a
Jianghai <marsandheart@gmail.com>
2016-08-08 09:44:02 +0800

    添加了接收默认选项功能

--- src/_component/base/PhoneInput.js
+++ src/_component/base/PhoneInput.js
@@ -6,23 +6,38 @@ class PhoneInput extends Component{
     constructor(props, context){
         super(props, context);
         this.state={
-            code_err:null
+            code_err:null,
+            value:null,
         }
     }
     change(e,value){
+        this.setState({value:value});
         let reg=/^[1][3578][0-9]{9}$/;
         if(reg.test(value)){
             this.setState({code_err:null});
+            if(value==this.props.old_value){
+                console.log('phone not change');
+                return;
+            }
+
             let _this=this;
             Wapi.user.checkExists(function(json){
                 if(json.exist){
                     _this.setState({code_err:___.phone_registed});
+                    _this.props.onChange(value,___.phone_registed);
                 }else{
                     _this.setState({code_err:null});
+                    _this.props.onChange(value);
                 }
             },{mobile:value});
         }else{
-            this.setState({code_err:___.phone_err});
+            if(value==''){
+                this.setState({code_err:___.phone_empty});
+                this.props.onChange(value,___.phone_empty);
+            }else{
+                this.setState({code_err:___.phone_err});
+                this.props.onChange(value,___.phone_err); 
+            }
         }
     }
     render(){
@@ -32,6 +47,7 @@ class PhoneInput extends Component{
                 errorText={this.state.code_err}
                 onChange={this.change.bind(this)}
                 style={this.props.style}
+                value={this.state.value||this.props.value}
             />
         );
     }
