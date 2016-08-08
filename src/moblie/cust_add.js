20e2221eda8df9d54473c1628bfda3e12d285709
Jianghai <marsandheart@gmail.com>
2016-08-08 18:15:29 +0800

    user_add.js改名为cust_add.js

--- src/moblie/cust_add.js
+++ src/moblie/cust_add.js
@@ -6,11 +6,16 @@ import {Provider,connect} from 'react-redux';
 import {ThemeProvider} from '../_theme/default';
 import AppBar from 'material-ui/AppBar';
 import UserAdd from '../_component/base/UserAdd';
+import STORE from '../_reducers/main';
+
 
 var thisView=window.LAUNCHER.getView();//第一句必然是获取view
 
 thisView.addEventListener('load',function(){
-    ReactDOM.render(<AppUserAdd/>,thisView);
+    ReactDOM.render(
+        <Provider store={STORE}>
+            <APP/>
+        </Provider>,thisView);
 });
 
 const _data={
@@ -30,10 +35,15 @@ const _data={
         }
 
 class AppUserAdd extends React.Component{
-    constructor(props){
-        super(props);
+    constructor(props,context){
+        super(props,context);
+    }
+    getChildContext(){
+        return {user_type:this.props.user_type};
     }
     render(){
+        console.log('AppUserAdd');
+        console.log(this.props);
         return(
             <ThemeProvider>
                 <div>
@@ -45,3 +55,13 @@ class AppUserAdd extends React.Component{
         );
     }
 }
+
+AppUserAdd.childContextTypes={
+    user_type: React.PropTypes.array
+}
+
+const APP=connect(function select(state) {
+    return {
+        user_type:state.user_type
+    };
+})(AppUserAdd);
