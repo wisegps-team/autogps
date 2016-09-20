716bded47ea5eb9af2961e9631f01a6f12dd6d41
wisegps <thomas@wisegps.cn>
2016-09-20 11:20:15 +0800

    用户管理添加搜索

--- src/moblie/user_manage.js
+++ src/moblie/user_manage.js
@@ -13,9 +13,12 @@ import STORE from '../_reducers/main';
 
 import Fab from '../_component/base/fab';
 import UserList from '../_component/userList';
-import Appbar from '../_component/base/appBar';
+import AppBar from '../_component/base/appBar';
+import IconButton from 'material-ui/IconButton';
+import ContentAdd from 'material-ui/svg-icons/content/add'
 
 import {userAct} from '../_reducers/customer';
+import UserSearch from '../_component/user_search';
 
 const thisView=window.LAUNCHER.getView();//第一句必然是获取view
 thisView.addEventListener('load',function(){
@@ -29,10 +32,14 @@ thisView.addEventListener('load',function(){
 class App extends Component{
     constructor(props, context) {
         super(props, context);
+        this.state={
+            search:false
+        }
         this._data={
             parentId:_user.customer.objectId,
             custTypeId:4
         };
+        this.onData = this.onData.bind(this);
     }
     getChildContext(){
         return{
@@ -50,13 +57,27 @@ class App extends Component{
     toAdd(){
         thisView.goTo('cust_add.js',{custTypeId:4});
     }
+
+    onData(data){
+        if(data&&data.length){
+            this._state={data,total:data.length};
+            this.setState({search:true});
+        }else{
+            this.setState({search:false});
+        }
+    }
     
     render() {
+        let list=this.state.search?(<UserList data={this._state.data} total={this._state.total}/>)
+            :(<UserList {...this.props.user}/>);
         return (
             <ThemeProvider>
+            <AppBar title={___.user_manage} iconElementRight={<IconButton onClick={this.toAdd}><ContentAdd/></IconButton>}/>
             <div>
-                <Fab onClick={this.toAdd}/>
-                <UserList {...this.props.user}/>
+                <div style={{padding:'0 10px'}}>
+                    <UserSearch onData={this.onData} data={this._data}/>
+                </div>
+                {list}
             </div>
             </ThemeProvider>
         );
