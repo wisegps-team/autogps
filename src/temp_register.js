bbc986956ab407f92f98c1a538f327ddec5f2137
wisegps <thomas@wisegps.cn>
2016-09-23 18:05:16 +0800

    登录优化，添加兼职营销人员注册页

---
+++ src/temp_register.js
@@ -0,0 +1,163 @@
+/**
+ * 2016-09-23
+ * 兼职营销人员注册页
+ */
+"use strict";
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import AppBar from 'material-ui/AppBar';
+import {ThemeProvider} from './_theme/default';
+import FlatButton from 'material-ui/FlatButton';
+
+import AgentRegisterBox from './_component/login/agent_register';
+import CONFIG from './_modules/config';
+
+import Register from './_component/login/register';
+import Input from './_component/base/input';
+import SexRadio from './_component/base/sexRadio';
+
+require('./_sass/index.scss');//包含css
+
+window.addEventListener('load',function(){
+    ReactDOM.render(<App/>,W('#main'));
+});
+
+
+class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.registerCallback = this.registerCallback.bind(this);
+    }
+
+    registerCallback(res){
+        if(!res._code){//注册成功
+            W.alert(___.register_success,()=>location='index.html');
+        }else{
+            switch (res._code) {
+                case 1:
+                    W.confirm(___.account_error);
+                    break;
+                default:
+                    W.alert(___.unknown_err);
+                    break;
+            }
+        }
+    }
+    render() {
+        let sty={padding:'10px'};
+        let data={
+            companyId:_g.parentId,
+            departId:-1,
+            role:'兼职营销',
+            roleId:'779209108162220000'
+        }
+        return (
+            <ThemeProvider>
+                <div className='login' style={sty}>
+                    <EmployeeRegisterBox 
+                        success={this.registerCallback} 
+                        defaultData={data}
+                    />
+                </div>
+            </ThemeProvider>
+        );
+    }
+}
+
+
+
+
+
+/**
+ * 接受的props：
+ *      parentId 新注册的员工的公司id
+ *      roleId 注册员工的角色
+ *      success 注册流程完成时的回调，未必注册成功
+ *          props.success(res) 传入一个对象res，根据res._code的值判断是否注册成功，
+ *          不存在或为0是注册成功，等于1为密码错误且之前已经注册过用户，
+ *          
+ */
+class EmployeeRegisterBox extends Component{
+    constructor(props, context) {
+        super(props, context);
+        this.registerSuccess = this.registerSuccess.bind(this);
+        this.data={
+            sex:1
+        };
+        Object.assign(this.data,props.defaultData);
+        this.change = this.change.bind(this);
+        this.nameChange = this.nameChange.bind(this);
+    }
+    
+    registerSuccess(res){
+        let user=res;
+        if(user.status_code){
+            //已注册用户，不能再注册了
+            W.alert(___.error[user.status_code]);
+            return;
+        }
+        if(!this.data.name){
+            W.alert(___.user_name_empty);
+            return;
+        }
+        W.loading(true);
+        let that=this;
+        let defaultData={
+            uid:user.uid,
+            tel:user.mobile,
+        }
+        let emp=Object.assign({},defaultData,this.data);
+        Wapi.user.login(function(data){//先登录获取token
+            if(data.status_code){
+                W.errorCode(data);
+                return;
+            }
+            delete data.status_code;
+            Object.assign(user,data);//用户信息
+            let token=data.access_token;
+            emp.access_token=token;
+            emp.uid=user.uid;
+            //添加人员表资料
+            Wapi.employee.add(function(res){
+                emp.objectId=res.objectId;
+                user.employee=emp;
+                Wapi.role.update(function(role){
+                    W.loading();
+                    user._code=0;
+                    that.props.success(user);
+                },{
+                    access_token:token,
+                    _objectId:emp.roleId,
+                    users:'+"'+emp.objectId+'"'
+                });
+            },emp);
+            Wapi.user.updateMe(null,{
+                _sessionToken:user.session_token,
+                access_token:token,
+                userType:9
+            });
+        },{
+            account:user.mobile,
+            password:user.password
+        });
+    }
+
+    nameChange(e,val){
+        this.data[e.target.name]=val;
+    }
+    change(val){
+        this.data.sex=val;
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <Input name='name' floatingLabelText={___.person_name} onChange={this.nameChange}/>
+                    <SexRadio onChange={this.change}/>
+                </form>
+                <Register onSuccess={this.registerSuccess}/>
+            </div>
+        );
+    }
+}
