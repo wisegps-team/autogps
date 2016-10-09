f013c491950672279aeb9c03e86735b7005c65c4
Jianghai <marsandheart@gmail.com>
2016-10-09 18:13:55 +0800

    角色管理页面完善

--- src/moblie/role_manage.js
+++ src/moblie/role_manage.js
@@ -5,9 +5,14 @@ import ReactDOM from 'react-dom';
 
 import {ThemeProvider} from '../_theme/default';
 import Card from 'material-ui/Card';
+import {List, ListItem} from 'material-ui/List';
+import FlatButton from 'material-ui/FlatButton';
+import Checkbox from 'material-ui/Checkbox';
 
 import AppBar from '../_component/base/appBar';
 import AutoList from '../_component/base/autoList';
+import SonPage from '../_component/base/sonPage';
+import Input from '../_component/base/input';
 
 const thisView=window.LAUNCHER.getView();//第一句必然是获取view
 thisView.addEventListener('load',function(){
@@ -17,11 +22,13 @@ thisView.addEventListener('load',function(){
 
 const styles={
     appbar:{position:'fixed',top:'0px',left:'0px'},
-    main:{width:'90%',paddingLeft:'5%',paddingRight:'5%',paddingTop:'60px',paddingBottom:'20px',},
+    main:{width:'90%',paddingLeft:'5%',paddingRight:'5%',paddingTop:'50px',paddingBottom:'20px'},
+    sonpage_main:{width:'90%',paddingLeft:'5%',paddingRight:'5%',paddingTop:'10px',paddingBottom:'20px'},
     card:{marginTop:'1em',padding:'0.5em'},
     td_left:{whiteSpace:'nowrap'},
     td_right:{paddingLeft:'1em'},
     bottom_btn_right:{width:'100%',display:'block',textAlign:'right',paddingTop:'5px'},
+    checkbox: {marginBottom: '16px'},
 }
 
 
@@ -30,28 +37,47 @@ class App extends React.Component {
         super(props, context);
         this.state={
             total:0,
-            roles:[],
+            show_sonpage:false,
         }
-        // this.roles=[];
+        this.roles=[];
+        this.addRole=this.addRole.bind(this);
+        this.addRoleCancel=this.addRoleCancel.bind(this);
+        this.addRoleSubmit=this.addRoleSubmit.bind(this);
     }
-    // getChildContext(){
-    //     return {
-    //     };
-    // }
     componentDidMount(){
         Wapi.role.list(res=>{
-            console.log(res);
-            // this.roles=res.data;
+            this.roles=res.data;
             this.setState({
                 total:res.total,
-                roles:res.data,
             });
         },{objectId:'>0'})
     }
-    loadNextPage(){
-        console.log('next page')
+    addRole(){
+        this.setState({show_sonpage:true});
+    }
+    addRoleCancel(){
+        this.setState({show_sonpage:false});
+    }
+    addRoleSubmit(data){
+        Wapi.role.add(res=>{//新建一个角色
+            let par={
+                _objectId:data.pages.join('|'),
+                ACL:'+role:'+res.objectId
+            }
+            Wapi.page.update(re=>{
+                //在所选的page的ACL中添加当前角色
+                this.roles.push({name:data.name});
+                this.setState({show_sonpage:false});
+            },par)
+
+        },{
+            name:data.name
+        });
     }
     render(){
+        let items=this.roles.map((ele,i)=>
+            <ListItem key={i} primaryText={ele.name}/>
+        );
         return(
             <ThemeProvider>
                 <div>
@@ -60,40 +86,63 @@ class App extends React.Component {
                         style={styles.appbar}
                     />
                     <div style={styles.main}>
-                        <Alist 
-                            max={this.state.total} 
-                            limit={20} 
-                            data={this.state.roles} 
-                            next={this.loadNextPage} 
-                        />
+                        <List>
+                            {items}
+                        </List>
+                        <div style={styles.bottom_btn_right}>
+                            <FlatButton label={___.add} primary={true} onClick={this.addRole} />
+                        </div>
                     </div>
+                    <SonPage title={"角色添加"} open={this.state.show_sonpage} back={this.addRoleCancel}>
+                        <AddRole submit={this.addRoleSubmit}/>
+                    </SonPage>
                 </div>
             </ThemeProvider>
         )
     }
 }
-// App.childContextTypes={
-// }
 
-
-class DumbList extends React.Component{
-    constructor(props,context){
-        super(props,context);
+class AddRole extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.data={
+            name:'',
+            pages:[],
+        }
+        this.nameChange=this.nameChange.bind(this);
+        this.pageCheck=this.pageCheck.bind(this);
     }
-    render() {
-        let cards=this.props.data.map((ele,i)=>{
-            <Card key={i} style={styles.card}>
-                <div>{ele.name}</div>
-            </Card>
-        })
+    nameChange(e,val){
+        this.data.name=val;
+    }
+    pageCheck(e,val){
+        if(val){
+            this.data.pages.push(e.target.name);
+        }else{
+            this.data.pages=this.data.pages.filter(ele=>ele!=e.target.name);
+        }
+    }
+    render(){
+        let pages=_user.pages;
+        let items=pages.map((ele,i)=>
+            <Checkbox
+                key={i}
+                name={ele.objectId.toString()}
+                label={ele.name}
+                style={styles.checkbox}
+                onCheck={this.pageCheck}
+            />
+        )
         return(
-            <div>
-                {cards}
+            <div style={styles.sonpage_main}>
+                <Input floatingLabelText={___.name} onChange={this.nameChange} />
+                <div style={{paddingBottom:'1em'}}>权限</div>
+                <div>{items}</div>
+
+                <div style={styles.bottom_btn_right}>
+                    <FlatButton label={___.ok} primary={true} onClick={()=>{this.props.submit(this.data)}} />
+                </div>
             </div>
         )
     }
 }
-//  DumbList.contextTypes={
-// };
-let Alist=AutoList(DumbList);
-
