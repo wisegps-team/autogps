b9d167ec509c945c80a92523ccb5c1e110dd171b
wisegps <thomas@wisegps.cn>
2017-01-13 15:02:46 +0800

    新增生成二维码组件

---
+++ src/_component/base/qrImg.js
@@ -0,0 +1,41 @@
+/**
+ * 传入一个text，生成二维码
+ */
+import React, {Component} from 'react';
+
+let jsLoad=function(){};
+W.include(WiStorm.root+'wslib/toolkit/qrcode.js',function(){
+    jsLoad();
+});
+
+class QrImg extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    componentDidMount() {
+        this.create();
+    }
+    
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.data!=this.props.data)
+            this.create();
+    }
+
+    create(){
+        let text=this.props.data;
+        if(QRCode)
+            new QRCode(this.refs.main,text);
+        else
+            jsLoad=()=>new QRCode(this.refs.main,text);
+    }
+    
+    render() {
+        return (
+            <div {... this.props} ref="main"></div>
+        );
+    }
+}
+
+export default QrImg;
