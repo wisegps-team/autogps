c087f73b4199f10a8953ac8fc0404e85731041d9
Jianghai <marsandheart@gmail.com>
2017-03-20 10:10:03 +0800

    更改两个暂时使用的config文件

--- src/_modules/Wapi/config.js
+++ src/_modules/Wapi/config.js
@@ -1,9 +1,15 @@
 import CONFIG from '../config';
 
+// const config={
+//     dev_key:CONFIG.devKey,
+//     app_key:CONFIG.appKey,
+//     app_secret:CONFIG.appSecret
+// }
+
 const config={
-    dev_key:CONFIG.devKey,
-    app_key:CONFIG.appKey,
-    app_secret:CONFIG.appSecret
+    dev_key:"86e3ddeb8db36cbf68f10a8b7d05e7ac",
+    app_key:"0642502f628a83433f0ba801d0cae4ef",
+    app_secret:"15fe3ee5197e8ba810512671483d2697"
 }
 
 
